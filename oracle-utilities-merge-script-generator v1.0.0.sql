6f0aad888aa463e3ac8ec2e246145432012c4a1b
Sheldon Bateman <58488695+smbatemanEA@users.noreply.github.com>
2025-05-20 00:43:00 -0500

    Update oracle-utilities-merge-script-generator v1.0.0.sql
    Added license to comment head

--- oracle-utilities-merge-script-generator v1.0.0.sql
+++ oracle-utilities-merge-script-generator v1.0.0.sql
@@ -15,6 +15,27 @@ CREATE OR REPLACE PROCEDURE ADMIN.GENERATE_MERGE_SCRIPT (
 					p_procedure_name  => 'PROC_MERGE_INTO_W1_ACTIVITY',
 					p_maint_obj_name  => 'W1-ASSET'
 				  )
+
+MIT License
+
+Copyright (c) 2025 Sheldon Bateman (Gravity Consulting, Github: smbatemanEA)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 ------------------------------------------------------------------------------------------------------------------------------
 Change History
 
